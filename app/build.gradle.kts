121dbdc7a9046e09078fc8c18789b1670404c773
Lucky31001 <kilian.dumaire@gmail.com>
2025-11-05 14:40:07 +0100

    add paper

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,4 +75,5 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.5")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.5")
+    implementation("io.github.pilgr:paperdb:2.7.2")
 }
--- app/src/main/java/com/example/scanner/MainActivity.kt
+++ app/src/main/java/com/example/scanner/MainActivity.kt
@@ -12,10 +12,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.scanner.ui.theme.ScannerTheme
+import io.paperdb.Paper
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Paper.init(this)
+
         enableEdgeToEdge()
         setContent {
             ScannerTheme {
