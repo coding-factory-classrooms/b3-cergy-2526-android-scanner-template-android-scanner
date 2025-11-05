eeefc625a75653c6c713da286c2e814b5ac70231
Matthieu LAURENT <ml.laurent.matthieu@gmail.com>
2025-11-05 15:05:26 +0100

    nfc readable

--- app/src/main/java/com/example/scanner/MainActivity.kt
+++ app/src/main/java/com/example/scanner/MainActivity.kt
@@ -1,47 +1,67 @@
 package com.example.scanner
 
+import android.nfc.NfcAdapter
+import android.nfc.Tag
+import android.nfc.tech.Ndef
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.scanner.ui.theme.ScannerTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(), NfcAdapter.ReaderCallback {
+    private lateinit var nfcAdapter: NfcAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            ScannerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this)
+
+        if (nfcAdapter == null) {
+            Toast.makeText(this, "NFC not supported", Toast.LENGTH_SHORT).show()
+            return
         }
+
+        if (!nfcAdapter.isEnabled) {
+            Toast.makeText(this, "Please turn on NFC", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "NFC found", Toast.LENGTH_SHORT).show()
+        }
+
+        nfcAdapter.enableReaderMode(
+            this,
+            this,
+            NfcAdapter.FLAG_READER_NFC_A or NfcAdapter.FLAG_READER_NFC_B,
+            null
+        )
+
+        setContent { ScannerTheme {} }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    override fun onTagDiscovered(tag: Tag?) {
+        Log.d("NFC", "Tag détecté : $tag")
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ScannerTheme {
-        Greeting("Android")
+        val ndef = Ndef.get(tag)
+
+        if(ndef!= null){
+            ndef.connect()
+
+            val ndefMessage = ndef.cachedNdefMessage
+            val records = ndefMessage.records
+            for (record in records){
+                val payload = record.payload
+
+                val text: String = payload.toString()
+
+                Log.d("NFC","Nfc data found: $text")
+            }
+            ndef.close()
+
+        }else{
+            Log.d("Error", "Ndef info not found")
+        }
     }
 }
--- app/src/main/java/com/example/scanner/scan/ScanActivity.kt
+++ app/src/main/java/com/example/scanner/scan/ScanActivity.kt
@@ -1,4 +1,29 @@
 package com.example.scanner.scan
 
-class ScanActivity {
+import android.nfc.NfcAdapter
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import com.example.scanner.R
+
+
+class ScanActivity: ComponentActivity () {
+    private lateinit var  nfcAdapter: NfcAdapter
+
+    override fun onCreate (savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+
+        // Get the default NFC adapter
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this)
+
+        if (nfcAdapter == null) {
+            Toast.makeText(this, "NFC not supported", Toast.LENGTH_SHORT).show()
+        } else if (!nfcAdapter!!.isEnabled) {
+            Toast.makeText(this, "Please turn on NFC", Toast.LENGTH_SHORT).show()
+        }else {
+            Toast.makeText(this, "NFC found", Toast.LENGTH_SHORT).show()
+        }
+
+
+    }
 }
